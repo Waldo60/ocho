26b833a2dc4cb54d3aee05bb6138ad0d8a2cd0fe
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-03-12 19:20:46 -0400

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -183,12 +183,12 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 function_verify () {
   permited=$(curl -sSL "https://raw.githubusercontent.com/Waldo60/ocho/master/Control-IP")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
-  echo -e "\n\n\n\033[1;95m======================================================\n ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR!,CONATACTE A @Rufu99\n======================================================\n"
+  echo -e "\n\n\n\033[1;95m======================================================\n ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR!,CONATACTE A @Test\n======================================================\n"
   [[ -d /etc/newadm ]] && rm -rf /etc/newadm
   exit 1
   } || {
   ### INTALAR VERCION DE SCRIPT
-  v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/VPS-MX-8.0/master/Vercion")
+  v1=$(curl -sSL "https://raw.githubusercontent.com/Waldo60/ocho/master/Vercion")
   echo "$v1" > /etc/versin_script
   }
 }
